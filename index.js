ab69a20d55a81fa8e68b6a31e7bac518a98c7e33
Phoscur <phoscur@pheelgood.net>
2023-12-25 11:45:50 +0100

    Rename public folder (assets)

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/node_modules/htmx.org/dist'));
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/public'));
 app.use(compression());
 
 app.get('/', (req, res) => {
